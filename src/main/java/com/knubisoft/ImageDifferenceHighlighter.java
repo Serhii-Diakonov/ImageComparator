bddeb591b5e4bd1442023dda78ceccb3f6cff13c
Serhii <sergeydyakonov222@gmail.com>
2022-09-09 10:16:23 +0300

    Fixed rectangles, refactoring is needed

--- src/main/java/com/knubisoft/ImageDifferenceHighlighter.java
+++ src/main/java/com/knubisoft/ImageDifferenceHighlighter.java
@@ -2,60 +2,99 @@ package com.knubisoft;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ImageDifferenceHighlighter {
 
     //Distance between points to add them into separate groups
-    private static int maxCapturingDistance = 200;
-    private static List<Point> differentPoints;
+    private int maxCapturingDistance = 200;
+    private Color highlightColor = new Color(255, 0, 0);
+    private Stroke stroke = new BasicStroke(4);
+    private int expandWidth = 10;
+    private int expandHeight = 10;
+    private List<Point> differentPoints;
 
-    public static BufferedImage highlightDifference(BufferedImage img1, BufferedImage img2) {
-        return highlightDifference(img1, img2, null);
+    public BufferedImage highlightDifference(BufferedImage defaultImg, BufferedImage changedImg) {
+        return highlightDifference(defaultImg, changedImg, null);
     }
 
-    public static BufferedImage highlightDifference(BufferedImage img1, BufferedImage img2, String newName) {
-        if (hasSameDimensions(img1, img2)) {
-            collectDifferentPoints(img1, img2);
+    public BufferedImage highlightDifference(BufferedImage defaultImg, BufferedImage changedImg, String newName) {
+        if (hasSameDimensions(defaultImg, changedImg)) {
+            collectDifferentPoints(defaultImg, changedImg);
             List<List<Point>> groups = splitPointsIntoGroups();
-            return highlightGroups(groups, img2);
+            return highlightGroups(groups, changedImg);
         } else {
             return null;
         }
     }
 
-    private static BufferedImage highlightGroups(List<List<Point>> groups, BufferedImage img2) {
+    public void setMaxCapturingDistance(int maxCapturingDistance) {
+        this.maxCapturingDistance = maxCapturingDistance;
+    }
+
+    public void setHighlightColor(Color highlightColor) {
+        this.highlightColor = highlightColor;
+    }
+
+    public void setStroke(Stroke stroke) {
+        this.stroke = stroke;
+    }
+
+    public void setExpandWidth(int expandWidth) {
+        this.expandWidth = expandWidth;
+    }
+
+    public void setExpandHeight(int expandHeight) {
+        this.expandHeight = expandHeight;
+    }
+
+    private BufferedImage highlightGroups(List<List<Point>> groups, BufferedImage img) {
         for (List<Point> group : groups) {
             int maxX = Collections.max(group, Comparator.comparingInt(o -> o.x)).x;
             int minX = Collections.min(group, Comparator.comparingInt(o -> o.x)).x;
             int maxY = Collections.max(group, Comparator.comparingInt(o -> o.y)).y;
-            int minY = Collections.max(group, Comparator.comparingInt(o -> o.y)).y;
-            Graphics2D graphics = img2.createGraphics();
-            graphics.setColor(new Color(255, 0, 0));
-            graphics.setStroke(new BasicStroke(4));
-            graphics.drawRect(minX, minY, maxX-minX, maxY-minY);
+            int minY = Collections.min(group, Comparator.comparingInt(o -> o.y)).y;
+            drawExpandedImageRegion(img, maxX, minX, maxY, minY);
         }
-        return img2;
+        return img;
     }
 
-    public static void setMaxCapturingDistance(int maxCapturingDistance) {
-        ImageDifferenceHighlighter.maxCapturingDistance = maxCapturingDistance;
+    private void drawExpandedImageRegion(BufferedImage img, int maxX, int minX, int maxY, int minY) {
+        Graphics2D graphics = img.createGraphics();
+        graphics.setColor(highlightColor);
+        graphics.setStroke(stroke);
+        minX -= expandWidth;
+        maxX += expandWidth;
+        minY -= expandHeight;
+        maxY += expandHeight;
+        graphics.drawRect(minX, minY, maxX - minX, maxY - minY);
     }
 
-    private static List<List<Point>> splitPointsIntoGroups() {
+    private List<List<Point>> splitPointsIntoGroups() {
+        List<Point> curGroup;
         List<List<Point>> groups = new ArrayList<>();
         groups.add(new ArrayList<>());
         for (Point point : differentPoints) {
-            for (List<Point> group : groups) {
-                for (Point groupedPoint : group) {
-                    if (groupedPoint.distance(point) < maxCapturingDistance) {
-                        group.add(point);
-                        break;
-                    } else if (group.indexOf(groupedPoint) == group.size() - 1) {
-                        List<Point> newGroup = new ArrayList<>();
-                        newGroup.add(point);
-                        groups.add(group);
+            for (int i = 0; i < groups.size(); i++) {
+                curGroup = groups.get(i);
+                if (curGroup.isEmpty()) {
+                    curGroup.add(point);
+                } else {
+                    for (Point groupedPoint : curGroup) {
+                        if (groupedPoint.distance(point) <= maxCapturingDistance) {
+                            if (!curGroup.contains(point)) {
+                                curGroup.add(point);
+                            }
+                            break;
+                        } else if (groups.get(groups.size() - 1) == curGroup &&
+                                curGroup.get(curGroup.size() - 1) == groupedPoint) {
+                            List<Point> newGroup = new ArrayList<>();
+                            newGroup.add(point);
+                            groups.add(newGroup);
+                        }
                     }
                 }
             }
@@ -63,18 +102,18 @@ public class ImageDifferenceHighlighter {
         return groups;
     }
 
-    private static void collectDifferentPoints(BufferedImage img1, BufferedImage img2) {
+    private void collectDifferentPoints(BufferedImage defaultImg, BufferedImage changedImg) {
         differentPoints = new ArrayList<>();
-        for (int y = 0; y < img1.getHeight(); y++) {
-            for (int x = 0; x < img1.getWidth(); x++) {
-                if (img1.getRGB(x, y) != img2.getRGB(x, y)) {
+        for (int y = 0; y < defaultImg.getHeight(); y++) {
+            for (int x = 0; x < defaultImg.getWidth(); x++) {
+                if (defaultImg.getRGB(x, y) != changedImg.getRGB(x, y)) {
                     differentPoints.add(new Point(x, y));
                 }
             }
         }
     }
 
-    private static boolean hasSameDimensions(BufferedImage img1, BufferedImage img2) {
+    private boolean hasSameDimensions(BufferedImage img1, BufferedImage img2) {
         return img1.getWidth() == img2.getWidth() && img1.getHeight() == img2.getHeight();
     }
 }
--- src/main/java/com/knubisoft/Main.java
+++ src/main/java/com/knubisoft/Main.java
@@ -3,61 +3,26 @@ package com.knubisoft;
 import lombok.SneakyThrows;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 
 public class Main {
 
-    @SneakyThrows
+//    @SneakyThrows
     public static void main(String[] args) {
-        Map<Point, Color> imgInfo1 = new LinkedHashMap<>();
-        Map<Point, Color> imgInfo2 = new LinkedHashMap<>();
-
-
         File file1 = new File("src/main/resources/test1.jpg");
         File file2 = new File("src/main/resources/test2.jpg");
-        BufferedImage inputImage1 = ImageIO.read(file1);
-        BufferedImage inputImage2 = ImageIO.read(file2);
-        BufferedImage result = ImageIO.read(file2);
-
-
-        ImageIO.write(ImageDifferenceHighlighter.highlightDifference(inputImage1, inputImage2), "jpg", new File("result.jpg"));
-
-       /* int firstX = inputImage1.getWidth(), firstY = inputImage1.getHeight();
-        int lastX = 0, lastY = 0;
-        Map<Point, Point> rectData = new LinkedHashMap<>();
-        for (Map.Entry<Point, Color> pointColorEntry : imgInfo1.entrySet()) {
-            if (!imgInfo2.get(pointColorEntry.getKey()).equals(pointColorEntry.getValue())) {
-                Point pixel = pointColorEntry.getKey();
-                if (pixel.x < firstX) {
-                    firstX = pixel.x;
-                }
-                if (pixel.y < firstY) {
-                    firstY = pixel.y;
-                }
-                if (pixel.x > lastX) {
-                    lastX = pixel.x;
-                }
-                if (pixel.y > lastY) {
-                    lastY = pixel.y;
-                }
+        try{
+            BufferedImage inputImage1 = ImageIO.read(file1);
+            BufferedImage inputImage2 = ImageIO.read(file2);
+            ImageDifferenceHighlighter highlighter = new ImageDifferenceHighlighter();
+            BufferedImage result = highlighter.highlightDifference(inputImage1, inputImage2);
+            if (result != null) {
+                ImageIO.write(result, "jpg", new File("result.jpg"));
             }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
 
-        Graphics2D graphics = result.createGraphics();
-        graphics.setColor(new Color(255, 0, 0));
-        graphics.setStroke(new BasicStroke(4));
-        drawExpandedRect(firstX, firstY, lastX, lastY, graphics);*/
-
-//        String imageExtension = file1.getName().split("\\.")[1];
-//        ImageIO.write(result, imageExtension, new File("result." + imageExtension));
-    }
-
-    private static void drawExpandedRect(int firstX, int firstY, int lastX, int lastY, Graphics2D graphics) {
-        graphics.drawRect(firstX - 5, firstY - 5, lastX - firstX + 10, lastY - firstY + 10);
     }
 }
